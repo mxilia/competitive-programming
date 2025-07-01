7004c7c2eab74a4659a7ff641fe2ff00b2c53691
mxilia <ravintkosol@gmail.com>
2025-07-01 11:59:32 +0700

    commit

--- codeforces/455A_Boredom.cpp
+++ codeforces/455A_Boredom.cpp
@@ -3,12 +3,18 @@
 using namespace std;
 using ll=long long;
 
-int a[100500];
+ll dp[100500],a[100500],f[100500],mx;
 
 signed main(){
     ios::sync_with_stdio(0), cin.tie(0);
     int n;
     cin >> n;
-    for(int i=1;i<=n;i++) cin >> a[i];
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+        f[a[i]]++;
+        mx=max(mx,a[i]);
+    }
+    for(int i=1;i<=mx;i++) dp[i]=max(dp[i-1],f[i]*i+dp[max(0,i-2)]);
+    cout << dp[mx];
     return 0;
 }

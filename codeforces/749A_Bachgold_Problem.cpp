9455d3187493690c29a5647cdbc7398bbbe4dca7
mxilia <ravintkosol@gmail.com>
2025-08-09 14:40:43 +0700

    Updated codefores.

--- codeforces/749A_Bachgold_Problem.cpp
+++ codeforces/749A_Bachgold_Problem.cpp
@@ -2,19 +2,16 @@
 #define newline cout << "\n";
 using namespace std;
 using ll=long long;
-const int inf=1e9+7;
-
-int s[2005];
-map<int,int> f;
 
 signed main(){
     ios::sync_with_stdio(0), cin.tie(0);
     int n;
     cin >> n;
-    for(int i=0;i<n;i++){
-        cin >> s[i];
-        f[s[i]]++;
+    cout << n/2; newline
+    while(n>3){
+        cout << '2' << ' ';
+        n-=2;
     }
-    cout << 1;
+    if(n) cout << n;
     return 0;
 }
